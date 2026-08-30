52a9b81dd2d8c18d470428a527b78a3475e0f0f3
Lyove <lyove@outlook.com>
2026-08-30 13:15:36 +0800

    init

--- src/layouts/base.layout.vue
+++ src/layouts/base.layout.vue
@@ -81,7 +81,7 @@ const tools = computed<ToolCategory[]>(() => [
           </div>
           <div>
             © {{ new Date().getFullYear() }}
-            <c-link target="_blank" rel="noopener" href="https://corentin.tech?utm_source=it-tools&utm_medium=footer">
+            <c-link target="_blank" rel="noopener" href="https://github.com/lyove/it-tools">
               Lyove
             </c-link>
           </div>
